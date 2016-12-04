769ae7cb709724d358727cb843365dd9a13ca547
NanoJoy <samuelevlafleur@gmail.com>
2016-12-04 17:58:53 -0500

    refactored studentwrapper tests

--- codebase/softwareeng/financeapp/tests.py
+++ codebase/softwareeng/financeapp/tests.py
@@ -10,32 +10,27 @@ class StudentTestCase(TestCase):
         saccount = Account.objects.create(user=suser, account_type="S", full_name="Test User")
         sstudent = StudentAccount.objects.create(account=saccount, funds=100)
         swrapper = StudentWrapper(sstudent)
-    
-    def test_get_full_name(self):
+
+    def get_student_wrapper(self):
         suser = User.objects.get(username="testuser")
         saccount = Account.objects.get(user=suser)
         sstudent = StudentAccount.objects.get(account=saccount)
         swrapper = StudentWrapper(sstudent)
+        return swrapper
+    
+    def test_get_full_name(self):
+        swrapper = self.get_student_wrapper()
         self.assertEqual(swrapper.get_full_name(), "Test User")
 
     def test_get_account(self):
-        suser = User.objects.get(username="testuser")
-        saccount = Account.objects.get(user=suser)
-        sstudent = StudentAccount.objects.get(account=saccount)
-        swrapper = StudentWrapper(sstudent)
+        swrapper = self.get_student_wrapper()
         self.assertEqual(swrapper.get_account(), saccount)
 
     def test_get_funds(self):
-        suser = User.objects.get(username="testuser")
-        saccount = Account.objects.get(user=suser)
-        sstudent = StudentAccount.objects.get(account=saccount)
-        swrapper = StudentWrapper(sstudent)
+        swrapper = self.get_student_wrapper()
         self.assertEqual(swrapper.get_funds(), 100)
     
     def test_deposit(self):
-        suser = User.objects.get(username="testuser")
-        saccount = Account.objects.get(user=suser)
-        sstudent = StudentAccount.objects.get(account=saccount)
-        swrapper = StudentWrapper(sstudent)
+        swrapper = self.get_student_wrapper()
         swrapper.deposit(100)
         self.assertEqual(swrapper.get_funds(), 200)
